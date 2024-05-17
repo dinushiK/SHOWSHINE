216dabb02b0e91b9b8f521428f388ac845206750
Dinushi Kolambathanthri <163441508+dinushiK@users.noreply.github.com>
2024-05-18 02:01:22 +0530

    Add files via upload

---
+++ w1953849_20210204/js/questions3.js
@@ -0,0 +1,115 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What is the k-drama about fencing sport?",
+    answer: "Twenty Five Twenty One",
+    options: [
+      "Twenty Five Twenty One",
+      "The glory",
+      "Island",
+      "Cheer Up"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Who is the director of Business Proposal K-drama?",
+    answer: "Park Seon Ho",
+    options: [
+      "Kim Sang Woo", 
+      "Song Hyeon Wook",
+      "Yoon Seong Shik",
+      "Park Seon Ho"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is the total of seasons in the Naruto original series and Naruto Shippuden series?",
+    answer: "26",
+    options: [
+      "5",
+      "220",
+      "21",
+      "26"
+    ]
+  },
+    {
+    numb: 4,
+    question: "In which year was the K-drama Goblin released?",
+    answer: "2016",
+    options: [
+      "2019",
+      "2018",
+      "2017",
+      "2016"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which character doesn't belong to the Demon Slayer series?",
+    answer: "Itachi Uchiha",
+    options: [
+      "Tanjiro Kamado",
+      "Itachi Uchiha",
+      "Zenitsu Agatsuma",
+      "Nezuko Kamado"
+    ]
+  },
+    {
+    numb: 6,
+    question: "Who is director of the Top Gun: Maverick?",
+    answer: "Joseph Kosinski",
+    options: [
+      "Martin Scorsese",
+      "Joseph Kosinski",
+      "Woody Allen",
+      "Spike Lee"
+    ]
+  },
+    {
+    numb: 7,
+    question: "Who is the actor who has not played Spider-Man?",
+    answer: "Jake Gyllenhaal",
+    options: [
+      "Tom Holland",
+      "Andrew Garfield",
+      "Tobey Maguire",
+      "Jake Gyllenhaal"
+      
+    ]
+  },
+    {
+    numb: 8,
+    question: "Who acted as the Joker in the Joker Hollywood movie?",
+    answer: "Joaquin Phoenix",
+    options: [
+      "Robert De Niro",
+      "Glenn Fleshler",
+      "Marc Maron",
+      "Joaquin Phoenix"
+
+    ]
+  },
+    {
+    numb: 9,
+    question: "At the 55th National Film Awards, Taare Zameen Par won 3 awards. Which award doesn't belong to the won awards?",
+    answer: "Best Actor",
+    options: [
+      "Best Actor",
+      "Best Film on Family Welfare",
+      "Best Lyrics",
+      "Best Male Playback Singer"
+    ]
+  },
+    {
+    numb: 10,
+    question: "In which year was the movie Kabir Singh released?",
+    answer: "2019",
+    options: [
+      "2020",
+      "2018",
+      "2019",
+      "2015"
+    ]
+  },
+];
